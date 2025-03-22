a1288fe25150705ccc81768f9c71397ed8aa146b
Joshua Peregrin <johnjoshuaperegrin2@gmail.com>
2025-03-22 22:47:31 +0800

    Accounts Page Header Created the accounts page header

--- src/views/Tab4Page.vue
+++ src/views/Tab4Page.vue
@@ -1,20 +1,78 @@
 <template>
   <ion-page>
     <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 3</ion-title>
-      </ion-toolbar>
+      <span class="title">My Account</span>
+      <ion-avatar>
+        <img :src="avatar_src"/>
+      </ion-avatar>
+      <span class="username">{{user_name}}</span>
+      <span class="subtext">{{user_email}}</span>
+      <span class="subtext">{{user_mobile}}</span>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 4</ion-title>
-        </ion-toolbar>
-      </ion-header>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup>
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+<style scoped>
+ion-header {
+  -webkit-box-shadow: none;
+  box-shadow: none;
+
+  background-color: var(--ion-color-primary);
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: nowrap;
+
+  padding-top: 45px;
+  padding-bottom: 15px;
+}
+.title {
+  font-size: x-large;
+  font-weight: 700;
+  color: var(--ion-color-primary-contrast)
+}
+.username {
+  font-size: medium;
+  font-weight: 700;
+  color: var(--ion-color-primary-contrast);
+  margin: 5px 0px;
+}
+.subtext {
+  font-size: xx-small;
+  font-weight: 400;
+  color: var(--ion-color-primary-contrast)
+}
+ion-avatar {
+  box-shadow: 1px 1px 25px -5px rgba(0,0,0,0.46);
+  height: 110px;
+  width: 110px;
+  margin-top: 15px;
+}
+</style>
+
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonAvatar } from '@ionic/vue';
+
+export default {
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonAvatar,
+  },
+  data(){
+    return {
+      avatar_src: "src/assets/imgs/angry.jpg",
+      user_name: "Washing Machine",
+      user_email: "washing.machine@gmail.com",
+      user_mobile: "+63-912-345-6789",
+    }
+  },
+}
 </script>
