9f253a92f3ac8d679120021a87cfcc33c6a1d113
Joshua Peregrin <johnjoshuaperegrin2@gmail.com>
2025-03-22 10:44:59 +0800

    Added icons and name to the navigation tab buttons Changed the icons and label of navigation tab buttons to: - home - services - invest - accounts

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -4,27 +4,42 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+          <ion-icon aria-hidden="true" src="src/assets/svgs/home.svg" />
+          <ion-label>Home</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+          <ion-icon aria-hidden="true" src="src/assets/svgs/bank.svg" />
+          <ion-label>Services</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon aria-hidden="true" :icon="square" />
-          <ion-label>Tab 3</ion-label>
+          <ion-icon aria-hidden="true" src="src/assets/svgs/invest.svg" />
+          <ion-label>Invest</ion-label>
         </ion-tab-button>
         <ion-tab-button tab="tab4" href="/tabs/tab4">
-          <ion-icon aria-hidden="true" :icon="square" />
-          <ion-label>Tab 3</ion-label>
+          <ion-icon aria-hidden="true" src="src/assets/svgs/account.svg" />
+          <ion-label>Account</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
   </ion-page>
 </template>
+
+<style scoped>
+ion-tab-bar {
+  height: 75px;
+  box-shadow: 0px -4px 14px rgba(0, 0, 0, 0.2);
+}
+ion-icon {
+  font-size: xx-large;
+  margin-bottom: 3px;
+}
+ion-label {
+  font-size: x-small;
+  margin-bottom: 0px ;
+}
+</style>
 
 <script setup>
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
