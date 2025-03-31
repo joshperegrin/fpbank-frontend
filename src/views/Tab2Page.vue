c1bc9298f37dbd84cd80b61c2376763ccb9d88fc
Joshua Peregrin <johnjoshuaperegrin2@gmail.com>
2025-03-31 19:37:36 +0800

    Header of Services Page Added the header of services page

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -1,20 +1,64 @@
 <template>
   <ion-page>
     <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
-      </ion-toolbar>
+      <ion-avatar>
+        <img :src="avatar_src"/>
+      </ion-avatar>
+      <span class="greetings"> {{first_name}}, </span>
+      <span class="greetings"> What would you like to do? </span> 
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
-        </ion-toolbar>
-      </ion-header>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup>
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+<style scoped>
+ion-header {
+  -webkit-box-shadow: none;
+  box-shadow: none;
+
+  background-color: var(--ion-color-primary);
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: nowrap;
+
+  padding-top: 45px;
+  padding-bottom: 15px;
+  border-radius: 0px 0px 16px 16px;
+}
+ion-avatar {
+  box-shadow: 1px 1px 25px -5px rgba(0,0,0,0.46);
+  height: 80px;
+  width: 80px;
+}
+ion-header span.greetings:first-of-type {
+  margin-top: 10px;
+}
+.greetings {
+  font-size: larger;
+  font-weight: 600;
+  color: var(--ion-color-primary-contrast);
+}
+</style>
+
+<script>
+import { IonPage, IonHeader, IonContent, IonAvatar } from '@ionic/vue';
+
+export default {
+  components: {
+    IonPage,
+    IonHeader,
+    IonContent,
+    IonAvatar
+  },
+  data(){
+    return {
+      avatar_src: "src/assets/imgs/angry.jpg",
+      first_name: "Washing",
+    }
+  },
+}
 </script>
